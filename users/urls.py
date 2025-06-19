4118cad08284644f9960320bc9ca976649d41633
Alexandra <a.chigrina89@gmail.com>
2025-06-19 17:14:21 +0300

    add settings

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+from users.apps import UsersConfig
+# from users.views import
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+
+]
