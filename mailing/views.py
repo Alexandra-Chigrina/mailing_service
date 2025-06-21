58bdc37d5977e49dfab15997fc663e271483c60e
Alexandra <a.chigrina89@gmail.com>
2025-06-21 16:36:38 +0300

    add mailing/views.py

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,149 @@
-from django.shortcuts import render
+from multiprocessing.resource_tracker import cleanup_noop
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView, View
+from django.urls import reverse_lazy
+
+from .models import Client, Message, Mailing
+
+
+class ClientListView(LoginRequiredMixin, ListView):
+    model = Client
+    template_name = 'mailing/client_list.html'
+    context_object_name ='clients'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Client.objects.filter(owner=self.request.user)
+
+
+class ClientDetailView(LoginRequiredMixin, DetailView):
+    model = Client
+    template_name = 'mailing/client_detail.html'
+    context_object_name = 'client'
+
+    def get_queryset(self):
+        return Client.objects.filter(owner=self.request.user)
+
+
+class ClientCreateView(LoginRequiredMixin, CreateView):
+    model = Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'mailing/client_form.html'
+    success_url = reverse_lazy('mailing:client_list')
+
+    def form_valid(self, form):
+        client = form.save()
+        user =self.request.user
+        client.owner = user
+        client.save()
+
+        return super().form_valid(form)
+
+
+class ClientUpdateView(LoginRequiredMixin, UpdateView):
+    model = Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'mailing/client_form.html'
+    success_url = reverse_lazy('mailing:client_list')
+
+
+class ClientDeleteView(LoginRequiredMixin, DeleteView):
+    model = Client
+    template_name = "mailing/client_confirm_delete.html"
+    success_url = reverse_lazy("mailing:client_list")
+
+
+class MessageListView(LoginRequiredMixin, ListView):
+    model = Message
+    template_name = 'mailing/message_list.html'
+    context_object_name ='messages'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Message.objects.filter(owner=self.request.user)
+
+
+class MessageDetailView(LoginRequiredMixin, DetailView):
+    model = Message
+    template_name = 'mailing/message_detail.html'
+    context_object_name = 'message'
+
+    def get_queryset(self):
+        return Message.objects.filter(owner=self.request.user)
+
+
+class MessageCreateView(LoginRequiredMixin, CreateView):
+    model = Message
+    fields = ['subject', 'body']
+    template_name = 'mailing/message_form.html'
+    success_url = reverse_lazy('mailing:message_list')
+
+    def form_valid(self, form):
+        client = form.save()
+        user =self.request.user
+        client.owner = user
+        client.save()
+
+        return super().form_valid(form)
+
+
+class MessageUpdateView(LoginRequiredMixin, UpdateView):
+    model = Message
+    fields = ['subject', 'body']
+    template_name = 'mailing/message_form.html'
+    success_url = reverse_lazy('mailing:message_list')
+
+
+class MessageDeleteView(LoginRequiredMixin, DeleteView):
+    model = Message
+    template_name = "mailing/message_confirm_delete.html"
+    success_url = reverse_lazy("mailing:message_list")
+
+
+class MailingListView(LoginRequiredMixin, ListView):
+    model = Mailing
+    template_name = 'mailing/mailing_list.html'
+    context_object_name ='mailings'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Mailing.objects.filter(owner=self.request.user)
+
+
+class MailingDetailView(LoginRequiredMixin, DetailView):
+    model = Mailing
+    template_name = 'mailing/mailing_detail.html'
+    context_object_name = 'mailing'
+
+    def get_queryset(self):
+        return Mailing.objects.filter(owner=self.request.user)
+
+
+class MailingCreateView(LoginRequiredMixin, CreateView):
+    model = Mailing
+    fields = ['start_time', 'end_time', 'status', 'message', 'clients']
+    template_name = 'mailing/mailing_form.html'
+    success_url = reverse_lazy('mailing:mailing_list')
+
+    def form_valid(self, form):
+        client = form.save()
+        user =self.request.user
+        client.owner = user
+        client.save()
+
+        return super().form_valid(form)
+
+
+class MailingUpdateView(LoginRequiredMixin, UpdateView):
+    model = Mailing
+    fields = ['start_time', 'end_time', 'status', 'message', 'clients']
+    template_name = 'mailing/mailing_form.html'
+    success_url = reverse_lazy('mailing:mailing_list')
+
+
+class MailingDeleteView(LoginRequiredMixin, DeleteView):
+    model = Mailing
+    template_name = "mailing/mailing_confirm_delete.html"
+    success_url = reverse_lazy("mailing:mailing_list")
